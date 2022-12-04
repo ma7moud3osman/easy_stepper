bb3dd883b938ebce7ee406b11c98cdfcf955db23
Dev1 <m.3osman99@gmail.com>
2022-12-04 03:11:14 +0200

    Update Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,26 @@
-import 'package:example/checkoutLayout/layout/checkout_layout_mobile.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:easy_stepper/easy_stepper.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int activeStep = 0;
+  final icons = [
+    const Icon(CupertinoIcons.cart),
+    const Icon(CupertinoIcons.info),
+    const Icon(CupertinoIcons.cart_fill_badge_plus),
+    const Icon(CupertinoIcons.money_dollar),
+    const Icon(Icons.check_circle_outline),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +30,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ScreenUtilInit(
-        builder: (context, child) => const CheckoutLayoutMobile(),
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              children: [
+                EasyStepper(
+                  activeStep: activeStep,
+                  steps: const [
+                    EasyStep(
+                      icon: Icon(CupertinoIcons.cart),
+                      title: 'Cart',
+                    ),
+                    EasyStep(
+                      icon: Icon(CupertinoIcons.info),
+                      title: 'Address',
+                    ),
+                    EasyStep(
+                      icon: Icon(CupertinoIcons.cart_fill_badge_plus),
+                      title: 'Checkout',
+                    ),
+                    EasyStep(
+                      icon: Icon(CupertinoIcons.money_dollar),
+                      title: 'Payment',
+                    ),
+                    EasyStep(
+                      icon: Icon(Icons.check_circle_outline),
+                      title: 'Finish',
+                    ),
+                  ],
+                  onStepReached: (index) => setState(() => activeStep = index),
+                ),
+                Expanded(
+                  child: Icon(
+                    icons[activeStep].icon,
+                    size: 180,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
